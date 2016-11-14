454d0700ec61cc994f97f2c6d7374e211642342c
bheltzel <Thysk9315>
2016-11-14 12:56:48 -0500

    Part 4 Completed

--- htime/polls/views.py
+++ htime/polls/views.py
@@ -8,6 +8,13 @@ from django.urls import reverse
 from django.template import loader
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
+
+
+def get_queryset(self):
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 
 class IndexView(generic.ListView):
@@ -15,12 +22,19 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
